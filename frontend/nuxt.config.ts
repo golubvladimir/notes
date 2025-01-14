7afea949da85f84e05672692a74e1a6e4ff530cc
Vladimir Golub <golubvlauri@gmail.com>
2025-01-14 23:48:42 +0300

    add route config for one page notes paths

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
+  hooks: {
+    'pages:extend'(routes) {
+      routes.push({
+        name: 'notes',
+        path: '/notes',
+        file: '~/pages/notes/[id].vue'
+      })
+    }
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_API_BASE || 'http://localhost:3000'
